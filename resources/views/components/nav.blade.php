84dde7c8446495829ff6e7d73965bb67159c9d47
sofiialorenzo <sofialorenzo1272@gmail.com>
2025-05-16 16:15:23 -0300

    Logo

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,7 +3,7 @@
     <nav class="bg-white w-full">
         <div class="max-w-screen-xl relative flex flex-row flex-wrap items-center justify-between mx-auto p-4">
             <a href="/" class="flex items-center space-x-3">
-                <img src="{{ asset('img/dalama-logo.png') }}" class="h-12" alt="Dalama Logo" />
+                <img src="{{ asset('img/dalama-logo.svg') }}" class="h-12" alt="Dalama Logo" />
             </a>
             <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
